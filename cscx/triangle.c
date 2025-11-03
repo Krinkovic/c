5acd77cc73d6e41cd844db02a308e0db1ef84c9d
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2025-11-03 15:37:05 +0100

    finished exercises to and including L4

--- cscx/triangle.c
+++ cscx/triangle.c
@@ -6,19 +6,19 @@ int main() {
 	int x, y, z, temp = 0;
 	while((scanf("%d %d %d", &x, &y, &z) != EOF)) {
   	if (y > x) {
-	    temp = x ;
-	    x = y ;
-	    y = temp ;
+	    temp = x;
+	    x = y;
+	    y = temp;
   	}
   	if (z > x) {
-	    temp = x ;
-	    x = z ;
-	    z = temp ;
+	    temp = x;
+	    x = z;
+	    z = temp;
   	}
 
-    if (x > y + z) {
+    if (x >= y + z) {
       printf("%s", "impossible\n");
-      continue ;
+      continue;
     }
     else if (x == y && y == z) {
       printf("%s", "equilateral ");
