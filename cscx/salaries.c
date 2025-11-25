da4a40f2f2963ab03ab4ae8233ea422ca8f03917
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2025-11-25 15:04:40 +0100

    update

---
+++ cscx/salaries.c
@@ -0,0 +1,62 @@
+// 2025 Kristoffer
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_NAME 60
+
+struct employee {
+    char name[MAX_NAME];
+    int salary;
+};
+
+int total_salary(const struct employee employees[], size_t n_employees);
+struct employee highest_salary(const struct employee employees[], size_t n);
+struct employee lowest_salary(const struct employee employees[], size_t n);
+
+int main() {
+  int nPeople = 0;
+
+  while (scanf(" %d", &nPeople) == 1) {
+    struct employee employees[nPeople];
+    for (int i = 0; i < nPeople; ++i) {
+      scanf(" %s %d", employees[i].name, &employees[i].salary);
+    }
+
+    struct employee highest = highest_salary(employees, nPeople);
+    struct employee lowest = lowest_salary(employees, nPeople);
+    int total = total_salary(employees, nPeople);
+
+    printf("%s has the highest salary: %d kr\n", highest.name, highest.salary);
+    printf("%s has the lowest salary: %d kr\n", lowest.name, lowest.salary);
+    printf("the monthly cost is %d\n", total);
+  }
+}
+
+int total_salary(const struct employee employees[], size_t n_employees) {
+  int total = 0;
+  for (int i = 0; i < n_employees; ++i) {
+    total += employees[i].salary;
+  }
+  return total;
+}
+
+struct employee highest_salary(const struct employee employees[], size_t n) {
+  struct employee highest = employees[0];
+  for (int i = 1; i < n; ++i) {
+    if (employees[i].salary > highest.salary) {
+      highest = employees[i];
+    }
+  }
+  return highest;
+}
+
+struct employee lowest_salary(const struct employee employees[], size_t n) {
+  struct employee lowest = employees[0];
+  for (int i = 1; i < n; ++i) {
+    if (employees[i].salary < lowest.salary) {
+      lowest = employees[i];
+    }
+  }
+  return lowest;
+  }
--- cscx/strange.c
+++ cscx/strange.c
@@ -8,7 +8,6 @@ char *strange(char s, char e);
 int main() {
   char start, end;
   while (scanf(" %c %c", &start, &end) == 2) {
-    printf("'%c' '%c'\n", start, end);
     char *charseq = strange(start, end);
     printf("%s\n", charseq);
     free(charseq);
@@ -17,7 +16,6 @@ int main() {
 
 char *strange(char s, char e) {
   size_t size = e - s + 1;
-  printf("size: %zu\n", size);
   char *str = malloc(size);
   for (int i = 0; i < size; ++i) {
     str[i] = s + i;
