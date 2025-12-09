d550dca0a6237b1a9b6112d3c16fc5e57dcfb20c
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2025-12-09 13:50:29 +0100

    more exercices

---
+++ cscx/lcm.c
@@ -0,0 +1,42 @@
+// 2025 Kristoffer
+
+#include <stdio.h>
+
+int lcm(int x, int y);
+int gcd(int x, int y);
+
+int main() {
+  int x, y;
+  while (scanf(" %d %d", &x, &y) == 2) {
+    printf("%d\n", lcm(x, y));
+  }
+}
+
+int lcm(int x, int y) {
+  return (x * y) / gcd(x, y);
+}
+
+int gcd(int x, int y) {
+  while (1) {
+    if (x == 0) {
+      return y;
+    }
+    if (y == 0) {
+      return x;
+    }
+
+    if (x >= y) {
+      x %= y;
+    } else {
+      y %= x;
+    }
+
+    if (x == 1) {
+      return x;
+    }
+    if (y == 1) {
+      return y;
+    }
+  }
+  return -1;
+}
