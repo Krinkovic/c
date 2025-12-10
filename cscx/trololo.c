07c93dad04c2e5be1b6f8954f72faed90c547edb
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2025-12-10 17:45:28 +0100

    Trololo

--- cscx/trololo.c
+++ cscx/trololo.c
@@ -5,40 +5,44 @@
 int main() {
   int bounce;
   int lo;
+  int preLo;
+  int postLo;
 
   while(scanf(" %d %d", &bounce, &lo) == 2) {
-    int preLo = 0;
-    int postLo = lo - 1;
     for (int i = 0; i < bounce; ++i) {
-      for (int i = 0; i < lo; ++i) {
-        printf("Tro");
-        for (int i = 0; i < preLo; ++i) {
-          printf("lo");
+      if (i % 2 == 0) {
+        preLo = 0;
+        postLo = lo - 1;
+        for (int i = 0; i < lo; ++i) {
+          printf("Tro");
+          for (int i = 0; i < preLo; ++i) {
+            printf("lo");
+          }
+          preLo += 1;
+          printf("\033[1;34mLO\033[0m");
+          for (int i = 0; i < postLo; ++i) {
+            printf("lo");
+          }
+          postLo -= 1;
+          puts("...");
         }
-        preLo += 1;
-        printf("\033[1;34mLO\033[0m");
-        for (int i = postLo; i > 0; --i) {
-          printf("lo");
+      } else {
+        preLo = lo - 1;
+        postLo = 0;
+        for (int i = 0; i < lo; ++i) {
+          printf("Tro");
+          for (int i = 0; i < preLo; ++i) {
+            printf("lo");
+          }
+          preLo -= 1;
+          printf("\033[1;34mLO\033[0m");
+          for (int i = 0; i < postLo; ++i) {
+            printf("lo");
+          }
+          postLo += 1;
+          puts("...");
         }
-        postLo -= 1;
-        puts("...");
       }
-
-      for (int i = 0; i < lo; ++i) {
-        printf("Tro");
-        for (int i = preLo; i >= 0; --i) {
-          printf("lo");
-        }
-        preLo -= 1;
-        printf("\033[1;34mLO\033[0m");
-        for (int i = 0; i < postLo; ++i) {
-          printf("lo");
-        }
-        postLo += 1;
-        puts("...");
-      }
-      // for (int i = 0; i < lo; ++ i) {
-      // }
     }
   }
 }
